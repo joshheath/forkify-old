da01ab4d950191ca013d303dc675c74ef719ca0d
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-12-13 14:40:23 +0000

    add api call framework

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,7 +1,19 @@
-// Global app controller
+import axios from 'axios';
 
-import x from './test'
+// https://www.food2fork.com/api/search
+// 8be3440ce9c8520a097af6981a258d04
 
-const y = 23;
+async function getResults(query) {
+  const key = '8be3440ce9c8520a097af6981a258d04';
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
 
-console.log(`I imported ${x} from another module called test.js. variable y is ${y}`);
+  try{
+    const recipes = res.data.recipes
+    console.log(recipes);
+  } catch(error) {
+    alert(error)
+  } 
+}
+
+getResults('pasta');
